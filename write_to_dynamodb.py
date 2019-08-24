4e2164f6f4cb8f0d5fa262f5e53e84a422697df4
kalaivendhanK <kalaisundaram93@gmail.com>
2019-08-25 01:01:39 +0530

    commiting the working post request

--- write_to_dynamodb.py
+++ write_to_dynamodb.py
@@ -1,10 +1,19 @@
 import requests
 
+# Below TODO to read the file contents and dynamically create body for post requests
+# It is now hardcoded to test body data
+# with open(file,rb) as f:
+
 url='https://k8wpr90m9d.execute-api.ap-south-1.amazonaws.com/dev/res1'
-params={
+body={
     "name": "test_name",
     "age":23,
     "qualification": "test_qualification"
 }
+headers = {'content-type': 'application/json'}
 
-requests.post(url=url,params=params)
+try:
+    data=requests.post(url=url,json=params,headers=headers)
+    print("Post Request Successfull...\n{}".format(data.text))
+except:
+    print("Requesting the AWS API Gateway endpoint filed with status...\n{}".format(data.text))
