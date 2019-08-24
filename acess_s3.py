c2337f7c8463a2de094c41f0c0eda375fb2eef43
kalaivendhanK <kalaisundaram93@gmail.com>
2019-08-24 16:24:07 +0530

    added logic to pull data from s3

--- acess_s3.py
+++ acess_s3.py
@@ -1,5 +1,6 @@
 import boto3
 import numpy as np
+import pandas as pd
 
 s3=boto3.resource('s3')
 
@@ -9,3 +10,7 @@ for bucket in s3.buckets.all():
 my_s3_bucket='sample-s3-bucket-for-me'
 data=open('upload_to_s3_test_file.txt','rb')
 s3.Bucket(my_bucket).put_object(Key='upload_to_s3_test_file.txt',Body=data)
+
+txt_object=s3.get_ohject(Bucket=my_s3_bucket,Key='upload_to_s3_test_file.txt')
+txt_data=pd.read_csv(txt_object['body'])
+print(txt_data)
